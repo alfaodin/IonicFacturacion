e015b239bd5708bc76c274a7680671c8a5e1ca00
Christian Cartes <alfaodin@gmail.com>
2018-07-05 08:40:27 -0500

    Estilos y features de SignUp

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, ToastController } from 'ionic-angular';
 
 import { User } from '../../providers/providers';
 import { MainPage } from '../pages';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -11,43 +12,45 @@ import { MainPage } from '../pages';
   templateUrl: 'signup.html'
 })
 export class SignupPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
-  };
-
+  newUserForm: FormGroup;
   // Our translated text strings
   private signupErrorString: string;
 
-  constructor(public navCtrl: NavController,
+  constructor(
     public user: User,
+    public navCtrl: NavController,
+    private formBuilder: FormBuilder,
     public toastCtrl: ToastController,
-    public translateService: TranslateService) {
-
-    this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
+    public translateService: TranslateService
+  ) {
+    this.translateService.get('SIGNUP_ERROR').subscribe(value => {
       this.signupErrorString = value;
-    })
+    });
+
+    this.newUserForm = this.formBuilder.group({
+      ruc: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   doSignup() {
     // Attempt to login in through our User service
-    this.user.signup(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
+    this.user.signup(this.newUserForm.value).subscribe(
+      resp => {
+        this.navCtrl.push(MainPage);
+      },
+      err => {
+        this.navCtrl.push(MainPage);
 
-      this.navCtrl.push(MainPage);
-
-      // Unable to sign up
-      let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    });
+        // Unable to sign up
+        let toast = this.toastCtrl.create({
+          message: this.signupErrorString,
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present();
+      }
+    );
   }
 }
